5dd30ba410fa521c10a1b7a3b5562788122f8790
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2022-05-30 20:22:38 +0300

    Added a navigation drawer

--- app/src/main/java/com/example/bottomnavkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavkotlin/MainActivity.kt
@@ -2,43 +2,83 @@ package com.example.bottomnavkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.close
+import android.system.Os.open
+import android.view.MenuItem
 import android.widget.TableLayout
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle : ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpTabBar()
+//        setUpTabBar()
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked Home",Toast.LENGTH_SHORT).show()
+                R.id.nav_share -> Toast.makeText(applicationContext, "Clicked share",Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked settings",Toast.LENGTH_SHORT).show()
+                R.id.nav_profile -> Toast.makeText(applicationContext, "Clicked ptofile",Toast.LENGTH_SHORT).show()
+                R.id.rate_us -> Toast.makeText(applicationContext, "Clicked rate us",Toast.LENGTH_SHORT).show()
+                R.id.login -> Toast.makeText(applicationContext, "Clicked login",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+       fun onOptionsItemSelected(item: MenuItem):Boolean{
+
+           if (toggle.onOptionsItemSelected(item)){
+
+               return true
+           }
+
+           return super.onOptionsItemSelected(item)
+       }
     }
 
-    private fun setUpTabBar() {
-        val adapter= TabPageAdapter(this,tabLayout.tabCount)
-        viewpager.adapter = adapter
+// Tab layout code
 
-        viewpager.registerOnPageChangeCallback(object:ViewPager2.OnPageChangeCallback()
-        {
-            override fun onPageSelected(position: Int) {
-                tabLayout.selectTab(tabLayout.getTabAt(position))
-            }
 
-        })
-        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                if (tab != null) {
-                    viewpager.currentItem=tab.position
-                }
-            }
+//    private fun setUpTabBar() {
+//        val adapter= TabPageAdapter(this,tabLayout.tabCount)
+//        viewpager.adapter = adapter
+//
+//        viewpager.registerOnPageChangeCallback(object:ViewPager2.OnPageChangeCallback()
+//        {
+//            override fun onPageSelected(position: Int) {
+//                tabLayout.selectTab(tabLayout.getTabAt(position))
+//            }
+//
+//        })
+//        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                if (tab != null) {
+//                    viewpager.currentItem=tab.position
+//                }
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//
+//            }
+//        })
+//    }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
-        })
-    }
 }
--- app/src/main/java/com/example/bottomnavkotlin/ProfileFragment.kt
+++ app/src/main/java/com/example/bottomnavkotlin/ProfileFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.*
 
 class ProfileFragment : Fragment() {
 
+    lateinit var toggle : ActionBarDrawerToggle
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
 }
